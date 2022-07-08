13c1760972003806cb286e445e438f84a126e8d0
Millena-Costa <Millena.Costa@ibm.com>
2022-07-08 19:44:29 -0300

    testando index

--- aulajpa/src/main/java/service/App.java
+++ aulajpa/src/main/java/service/App.java
@@ -1,5 +1,8 @@
 package service;
 
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
