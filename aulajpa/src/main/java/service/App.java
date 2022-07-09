8fef6c9d7e58e5bd38f42bcd6c03aa1cb1c95197
Millena-Costa <Millena.Costa@ibm.com>
2022-07-08 21:31:22 -0300

    full RestAPI

--- aulajpa/src/main/java/service/App.java
+++ aulajpa/src/main/java/service/App.java
@@ -1,8 +1,5 @@
 package service;
 
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.Response;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
